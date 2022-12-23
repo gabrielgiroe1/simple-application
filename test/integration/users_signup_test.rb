48efd0f85becf2289386bb57363d91fbf588ab70
Georgel Gabriel Giroe <gabriel.giroe@gmail.com>
2022-12-23 11:09:54 +0200

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -9,9 +9,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, params: { user: { name: "",
-                                          email: "email@invalid",
-                                          password: "foo",
-                                          password_confirmation: "bar"
+                                         email: "email@invalid",
+                                         password: "foo",
+                                         password_confirmation: "bar"
       } }
     end
     assert_template 'users/new'
@@ -22,15 +22,15 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "valid sign-up information" do
     get signup_path
     assert_difference 'User.count', 1 do
-      post users_path, params: {user:{ name: "gabriel",
-                                       email: "gabriel.giroe@ulbsibiu.ro",
-                                       password: "123456",
-                                       password_confirmation: "123456"
-      }}
+      post users_path, params: { user: { name: "gabriel",
+                                         email: "gabriel.giroe@ulbsibiu.ro",
+                                         password: "123456",
+                                         password_confirmation: "123456"
+      } }
     end
     follow_redirect!
     assert_template 'users/show'
     assert_not flash.nil?
+    assert is_logged_in?
   end
 end
-§
