93be8672c5d74d64982ea5ab417a7ef0fbb3cadc
Georgel Gabriel Giroe <gabriel.giroe@gmail.com>
2023-01-05 18:41:09 +0200

    Implement advanced login but with some integration test error

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -15,8 +15,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       } }
     end
     assert_template 'users/new'
-    # assert_select 'div#<CSS id for error explanation>'
-    # assert_select 'div.<CSS class for field with error>'
   end
 
   test "valid sign-up information" do
