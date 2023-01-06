46db0cabaf7db493fbb719c0892bf976f2177cc1
Georgel Gabriel Giroe <gabriel.giroe@gmail.com>
2023-01-06 23:14:19 +0200

    Requiring the right user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   before_action :set_users, only: %i[show edit update destroy]
 
   def show
@@ -33,9 +35,22 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = 'Successful update'
+      redirect_to @user
     else
       render 'edit'
     end
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = 'Please log in.'
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
   end
 
   private
