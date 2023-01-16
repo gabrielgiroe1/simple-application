b3baa21d73128c595d18341e902af61d6bc0d659
Georgel Gabriel Giroe <gabriel.giroe@gmail.com>
2023-01-16 16:38:10 +0200

    Completed password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # get 'sessions/new'
-  #  get 'users/new'
   root "static_pages#home"
   get '/about', to: 'static_pages#about'
   get '/help', to: 'static_pages#help'
@@ -11,6 +9,7 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :edit, :create, :update]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
