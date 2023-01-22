06aaaa1c951d5f22b83c37ac2b9607fa05893c57
Georgel Gabriel Giroe <gabriel.giroe@gmail.com>
2023-01-22 20:11:39 +0200

    Micropost images

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  # scope :microposts, -> { order(created_at: desc)}
+  has_one_attached :image
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 140}
