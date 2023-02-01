5342c015ea382c61ccd1fa0f1995a5166cb07133
Georgel Gabriel Giroe <gabriel.giroe@gmail.com>
2023-02-01 17:42:42 +0200

    Add user status feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -66,10 +66,12 @@ class User < ApplicationRecord
   def feed
     # Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
     #  Micropost.where("user_id IN (:following_ids) OR user_id = :user_id", following_ids: following_ids, user_id: id)
-    following_ids = "SELECT followed_id FROM relationships
-                      WHERE follower_id = :user_id"
-    Micropost.where("user_id IN (#{following_ids})
-                     OR user_id = :user_id", user_id: id)
+    # following_ids = "SELECT followed_id FROM relationships
+    #                   WHERE follower_id = :user_id"
+    # Micropost.where("user_id IN (#{following_ids})
+    #                  OR user_id = :user_id", user_id: id)
+    part_of_feed = "relationships.follower_id = :id or microposts.user_id = :id"
+    Micropost.joins(user: :followers).where(part_of_feed, {id: id})
   end
 
   def follow(other_user)
